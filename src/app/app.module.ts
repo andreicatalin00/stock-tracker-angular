df6fbdfaa393d79179aa2b0c5638d6ba282f4cee
andreicatalin00 <catalinmilitarescu00@gmail.com>
2022-10-26 15:05:51 +0200

    Added styling to stock symbol search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatIconModule} from "@angular/material/icon";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {AppComponent} from './app.component';
-import {StockSymbolSearchComponent} from './components/stock-symbol-search/stock-symbol-search.component';
+import { AppComponent } from './app.component';
+import { StockSymbolSearchComponent } from './components/stock-symbol-search/stock-symbol-search.component';
 
 @NgModule({
-    imports: [BrowserModule, FormsModule, MatInputModule, MatIconModule],
-    declarations: [AppComponent, StockSymbolSearchComponent],
-    bootstrap: [AppComponent]
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatInputModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatButtonModule,
+  ],
+  declarations: [AppComponent, StockSymbolSearchComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
