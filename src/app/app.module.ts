4428e0ae2d58549d6468b6e258c4d7049631045a
catalinmilitarescu <catalinmilitarescu00@gmail.com>
2022-10-26 13:52:58 +0200

    Added Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from "@angular/material/icon";
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {StockSymbolSearchComponent} from './components/stock-symbol-search/stock-symbol-search.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+    imports: [BrowserModule, FormsModule, MatInputModule, MatIconModule],
+    declarations: [AppComponent, StockSymbolSearchComponent],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
