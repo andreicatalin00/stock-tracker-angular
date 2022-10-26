feee31f729a5419457749597d929c506bf8ae7a2
andreicatalin00 <catalinmilitarescu00@gmail.com>
2022-10-26 17:07:51 +0200

    Added max, min and pattern validation

--- src/app/components/stock-symbol-search/stock-symbol-search.component.ts
+++ src/app/components/stock-symbol-search/stock-symbol-search.component.ts
@@ -10,12 +10,21 @@ export class StockSymbolSearchComponent implements OnInit {
   public symbolCodeControl: FormControl = new FormControl('', [
     Validators.required,
     Validators.maxLength(5),
-    Validators.minLength(1),
+    Validators.pattern('^[a-zA-Z]+$'),
   ]);
 
   ngOnInit(): void {}
 
   public trackSymbol(): void {
     console.log(this.symbolCodeControl.value);
+  }
+  public getErrorMessage(): string {
+    console.log(this.symbolCodeControl.errors);
+    if (this.symbolCodeControl.hasError('maxlength')) {
+      return 'Should have maximum 5 letters';
+    } else if (this.symbolCodeControl.hasError('pattern')) {
+      return 'Should have only letters';
+    }
+    return 'Should have minimum 1 letter';
   }
 }
