220b651c3c9fffca47e87654302e703a52da17e9
andreicatalin00 <catalinmilitarescu00@gmail.com>
2022-10-28 22:18:31 +0200

    Small fix routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppModule } from './app.module';
 import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [{ path: '*', component: HomeComponent }];
+const routes: Routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
